45778359dee17332e6f403eb48fd5836764d1a2b
Victoria Phelps <jedi_force@berkeley.edu>
2024-07-22 12:38:59 -0700

    Reformatting the files

--- index.md
+++ index.md
@@ -6,3 +6,152 @@ has_children: true
 has_toc: true
 ---
 
+# Blocks, Scripts, and Sprites 
+### Hat Blocks and Command Blocks 
+## Sprites and Parallelism  
+### Costumes and Sounds 
+### Inter-Sprite Communication with Broadcast  
+## Nesting Sprites: Anchors and Parts  
+## Reporter Blocks and Expressions  
+## Predicates and Conditional Evaluation 
+## Variables 
+### Global Variables 
+### Script Variables  
+### Renaming variables  
+### Transient variables 
+## Debugging 
+### The pause button 
+### Breakpoints: the pause all block  
+### Visible stepping 
+## Etcetera  
+## Libraries 
+
+# Saving and Loading Projects and Media  
+## Local Storage  
+## Creating a Cloud Account 
+## Saving to the Cloud 
+## Loading Saved Projects 
+## If you lose your project, do this first! 
+### Private and Public Projects  
+
+# Building a Block 
+## Simple Blocks 
+### Custom Blocks with Inputs 
+### Editing Block Properties  
+## Recursion 
+## Block Libraries 
+## Custom blocks and Visible Stepping 
+
+# First Class Lists  
+## The List Block 
+## Lists of Lists 
+## Functional and Imperative List Programming 
+## Higher Order List Operations and Rings 
+## Table View vs List View 
+### Comma-Separated Values 
+### Multi-dimensional Lists and JSON 
+## Hyperblocks 
+
+# Typed Inputs 
+## Scratch’s Type Notation  
+## The Snap! Input Type Dialog 
+### Procedure Types 
+### Pulldown Inputs  
+### Input Variants 
+### Prototype Hints 
+### Title Text and Symbols  
+
+# Procedures as Data 
+## Call and Run 
+### Call/Run with Inputs 
+### Variables in Ring Slots  
+## Writing Higher Order Procedures 
+### Recursive Calls to Multiple-Input Blocks 
+## Formal Parameters  
+## Procedures as Data  
+## Special Forms  
+### Special Forms in Scratch  
+
+# Object Oriented Programming with Sprites
+## First Class Sprites 
+## Permanent and Temporary Clones 
+## Sending Messages to Sprites 
+### Polymorphism 
+## Local State in Sprites: Variables and Attributes 
+## Prototyping: Parents and Children  
+## Inheritance by Delegation 
+## List of Attributes 
+## First Class Costumes and Sounds 
+### Media Computation with Costumes 
+### Media Computation with Sounds 
+
+# OOP with Procedures 
+## Local State with Script Variables  
+## Messages and Dispatch Procedures 
+## Inheritance via Delegation 
+## An Implementation of Prototyping OOP 
+
+# The Outside World
+## The World Wide Web  
+## Hardware Devices  
+## Date and Time 
+
+# Continuations
+## Continuation Passing Style  
+## Call/Run with Continuation  
+### Nonlocal Exit 
+
+# Metaprogramming 
+## Reading a Block 
+## Writing a Block 
+## Macros 
+
+# User Interface Elements  
+## Tool Bar Features  
+### The Snap! Logo Menu 
+### The File Menu 
+### The Cloud Menu 
+### The Settings Menu  
+### Visible Stepping Controls 
+### Stage Resizing Buttons  
+### Project Control Buttons 
+## The Palette Area  
+### Buttons in the Palette 
+### Context Menus for Palette Blocks 
+### Context Menu for the Palette Background 
+## The Scripting Area  
+### Sprite Appearance and Behavior Controls 
+### Scripting Area Tabs 
+### Scripts and Blocks Within Scripts 
+### Controls in the Costumes Tab 
+### The Paint Editor 
+### Controls in the Sounds Tab 
+## Keyboard Editing 
+### Starting and Stopping the Keyboard Editor  
+### Navigating in the Keyboard Editor 
+### Editing a Script 
+### Running the Selected Script 
+## Controls on the Stage 
+### Sprites  
+### Variable Watchers  
+### The Stage Itself  
+## The Sprite Corral and Sprite Creation Buttons 
+## Preloading a Project when Starting Snap! 
+## Mirror Sites 
+
+# Appendix A. Snap! Color Library 
+### Introduction to Color 
+### Crayons and Color Numbers 
+### Perceptual Spaces: HSV and HSL 
+### Mixing Colors 
+### tl;dr 
+#### Subappendix: Geeky Details on Fair Hue  
+#### Subappendix: Geeky Details on Color Numbers  
+
+# Appendix B. APL Features 
+### Boolean Values  
+### Scalar Functions 
+### Mixed Functions  
+### Higher Order Functions 
+
+# Index
--- parse-md-file.rb
+++ parse-md-file.rb
@@ -1,7 +1,8 @@
-INCH_TO_PIXEL = 96
+# Constants for conversion
+$INCH_TO_PIXEL = 96
 
-index_file = "index.md"
-output_file = "output2.md"
+$chapter_subsections = ["Hat Blocks and Command Blocks", "Introduction to Color", "Crayons and Color Numbers", "Perceptual Spaces: HSV and HSL", "Mixing Colors", "tl;dr", "Subappendix: Geeky Details on Fair Hue", "Subappendix: Geeky Details on Color Numbers", "Boolean Values", "Scalar Functions", "Mixed Functions", "Higher Order Functions"]
+$path = "C:/Users/victo/snap-manual/test/"
 
 def parse_toc(index_file)
   toc = []
@@ -9,123 +10,139 @@ def parse_toc(index_file)
   current_section = nil
 
   File.foreach(index_file) do |line|
+    
     case line
-    when /^# (.*)$/
-      current_chapter = { title: $1.strip, sections: [] }
-      toc << current_chapter
-    when /^## (.*)$/
-      current_section = { title: $1.strip, subsections: [] }
-      current_chapter[:sections] << current_section if current_chapter
-    when /^### (.*)$/
-      current_section[:subsections] << $1.strip if current_section
+    when /^#(.*)$/
+      toc.append(line)
     end
+  #     #current_chapter += line #{ title: $1.strip, sections: [] }
+  #     #toc << current_chapter
+  #   when /^## (.*)$/
+  #     current_section = #{ title: $1.strip, subsections: [] }
+  #     #current_chapter << current_section if current_chapter
+  #   when /^### (.*)$/
+  #     #current_section << $1.strip if current_section
+  #   end
+  # end
   end
-
-  toc
-end
-
-toc = parse_toc(index_file)
-all_lines = File.readlines(output_file)
-
-def create_new_files(all_lines, toc)
-  current_chapter = nil
-  current_section = nil
-  current_subsection = nil
-  chapter_content = ""
-  section_content = ""
-
-  toc.each_with_index do |chapter, chapter_index|
-    current_chapter = chapter[:title]
-    chapter_filename = "C#{chapter_index + 1}.md"
-    chapter_content = <<-TEXT
----
-title: #{current_chapter}
-layout: home
-has_children: true
-has_toc: false
-nav_order: #{chapter_index + 1}
----
-
-TEXT
-
-    section_content = ""
-    subsection_content = ""
-
-    all_lines.each_with_index do |line, i|
-      line = convert_image_dimensions(line) if line.match?(/width="([\d.]+)in"/) || line.match?(/height="([\d.]+)in"/)
-
-      if line.match(/^# #{Regexp.quote(current_chapter)}/)
-        next_line = all_lines[i + 1]
-        if next_line && next_line.match(/^## /)
-          current_section = chapter[:sections].find { |section| next_line.include?(section[:title]) }
-        end
-        next
-      elsif current_section && line.match(/^## #{Regexp.quote(current_section[:title])}/)
-        section_filename = "C#{chapter_index + 1}-Sec#{chapter[:sections].index(current_section) + 1}.md"
-        section_content = <<-TEXT
----
-title: #{current_section[:title]}
-layout: home
-parent: #{current_chapter}
-has_children: #{current_section[:subsections].any?}
-has_toc: true
-nav_order: #{chapter[:sections].index(current_section) + 1}
----
-
-TEXT
-        next_line = all_lines[i + 1]
-        if next_line && next_line.match(/^### /)
-          current_subsection = current_section[:subsections].find { |sub| next_line.include?(sub) }
-        end
-        next
-      elsif current_subsection && line.match(/^### #{Regexp.quote(current_subsection)}/)
-        subsection_content = <<-TEXT
----
-title: #{current_subsection}
-layout: home
-parent: #{current_section[:title]}
-grand_parent: #{current_chapter}
-has_toc: false
-nav_order: #{current_section[:subsections].index(current_subsection) + 1}
----
-
-TEXT
-        section_content += subsection_content
-        next
-      end
-
-      if current_subsection
-        section_content += line
-      elsif current_section
-        section_content += line
-      else
-        chapter_content += line
-      end
-    end
-
-    File.open(chapter_filename, "w") { |f| f.write(chapter_content) }
-
-    chapter[:sections].each_with_index do |section, section_index|
-      section_filename = "C#{chapter_index + 1}-Sec#{section_index + 1}.md"
-      File.open(section_filename, "w") { |f| f.write(section_content) }
-    end
-  end
-end
-
-def convert_image_dimensions(line)
-  line.gsub!(/width="([\d.]+)in"/) do |match|
-    pixels = inch_to_pixel($1)
-    "width=\"#{pixels}\""
-  end
-  line.gsub!(/height="([\d.]+)in"/) do |match|
-    pixels = inch_to_pixel($1)
-    "height=\"#{pixels}\""
-  end
-  line
+  return toc
 end
 
 def inch_to_pixel(inch)
-  (inch.to_f * INCH_TO_PIXEL).round
+  (inch.to_f * $INCH_TO_PIXEL).round
 end
 
-create_new_files(all_lines, toc)
+def convert_image_dimensions(line)
+  line.gsub(/!\[\]\(([^)]+)\)\{width="([\d.]+)in" height="([\d.]+)in"\}/) do
+    image_path = $1
+    width_in_inch = $2.to_f
+    height_in_inch = $3.to_f
+    width_in_px = inch_to_pixel(width_in_inch)
+    height_in_px = inch_to_pixel(height_in_inch)
+    "<img src=\"/snap-manual/assets/images/#{File.basename(image_path)}\" style=\"width:#{width_in_px}px; height:#{height_in_px}px\">"
+  end
+end
+
+def remove_hashtags(string)
+  # Use gsub to replace hashtags followed by a space with an empty string
+  return string.gsub(/#+\s*/, '')
+end
+
+def create_new_files(all_lines)
+  num, name, file, chapter_content = 0, "", "", ""
+  current_chapter_title = ""
+  chapter_num = 0
+  section_content, section_num = "", 0
+  write_chapter = false
+  write_section = false
+  intialize = true
+  chapter_filename = ""
+  section_filename = ""
+  all_lines.each_with_index do |line, i|
+
+    if line.match?(/width="([\d.]+)in"/) || line.match?(/height="([\d.]+)in"/)
+      line = convert_image_dimensions(line)
+    end
+
+    $toc.each_with_index do |section|
+      no_hash_section = remove_hashtags(section)
+      if section.match?(/^(\# )/) && line.include?(no_hash_section)
+        puts "Line include no hash?: #{line.include?(no_hash_section)}"
+        puts line
+        puts "\n" 
+        if not(intialize) and chapter_filename != ""
+          File.open($path + chapter_filename, "w") { |f| f.write(chapter_content) }
+        end
+        intialize = false
+        current_chapter_title = no_hash_section
+        chapter_filename = "C#{chapter_num}.md"
+        write_chapter = true
+        write_section = false
+        num += 1
+        chapter_num += 1
+        chapter_content = <<-TEXT
+        ---
+        title: Chapter #{chapter_num}. #{current_chapter_title}
+        layout: home
+        has_children: true
+        has_toc: true
+        nav_order: #{num + 1}
+        ---
+
+        TEXT
+        puts current_chapter_title
+        #File.open($path + chapter_filename, "w") { |f| f.write(chapter_content) }
+        break
+      
+
+      elsif section.match?(/\#\# /) && line.include?(no_hash_section)
+        if section_filename != ""
+          File.open($path + section_filename, "w") { |f| f.write(section_content) }
+        end
+        section_num += 1
+        section_title = no_hash_section
+        section_filename = "C#{chapter_num}-S#{section_num}.md"
+        section_num += 1
+        write_section = true
+        write_chapter = false
+        name = all_lines[i-1].gsub(/!\[.*\]\(.*\)\{.*\}/, '').strip
+        num += 1
+        section_content = <<-TEXT
+        ---
+        title: #{section_num}. #{section_title}
+        layout: home
+        has_children: false
+        has_toc: true
+        nav_order: #{num + 1}
+        parent: #{current_chapter_title}
+        ---
+
+        TEXT
+        #File.open($path + section_filename, "w") { |f| f.write(section_content) }
+        break
+
+      # elsif section.include?("###")
+      #   if $chapter_subsections.include?(no_hash_section)
+      #     chapter_content += "#{no_hash_section}"
+      #   else
+      #     section_content += "#{no_hash_section}"
+      #   end
+      end
+    
+    end
+  
+    
+
+    if write_chapter
+      chapter_content += line
+    else write_section
+      section_content += line
+    end
+  end
+  
+end
+
+outputFile = File.new("output2.md", "r")
+all_lines = File.readlines(outputFile)
+$toc = parse_toc("toc.txt")
+create_new_files(all_lines)
